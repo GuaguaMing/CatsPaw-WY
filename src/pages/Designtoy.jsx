c8958228a02b899bdb61700c7794ef036aa38393
Guaguaming <pantxiketsai499@gmail.com>
2025-06-13 08:48:41 +0800

    0613施工

--- src/pages/Designtoy.jsx
+++ src/pages/Designtoy.jsx
@@ -4,8 +4,8 @@ import '../index.css';
 const Designtoy = () => {
   return (
     <div className="flex flex-col items-center justify-center bg-black text-white">
-      <h1 className="text-4xl font-bold mb-4">公仔分頁</h1>
-      <p className="text-lg">這是公仔分頁的內容。</p>
+      <h1 className="text-4xl font-bold mb-4">Wywy設計師玩具</h1>
+      <p className="text-lg">施工中...詳情可見instagram</p>
     </div>
   );
 };
--- src/pages/Scene.jsx
+++ src/pages/Scene.jsx
@@ -4,8 +4,8 @@ import '../index.css';
 const Scene = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
-      <h1 className="text-4xl font-bold mb-4">場景分頁</h1>
-      <p className="text-lg">這是場景分頁的內容。</p>
+      <h1 className="text-4xl font-bold mb-4">遊戲場景</h1>
+      <p className="text-lg">施工中...</p>
     </div>
   );
 };
